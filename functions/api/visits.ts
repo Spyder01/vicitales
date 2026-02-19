34eb2e248ae452fa708be00006800bc87a0ea055
Suhan J Bangera <s.bangera@castsoftware.com>
2026-02-20 02:51:20 +0530

    Added analytics

--- functions/api/visits.ts
+++ functions/api/visits.ts
@@ -7,47 +7,59 @@
  */
 
 export async function onRequestPost(context: any) {
-  const { request, env } = context;
+    const { request, env } = context;
 
-  try {
-    const body = await request.json();
+    try {
+        const ua = request.headers.get("user-agent") || "";
 
-    const path: string = body.path || "/";
-    const sessionId: string | null = body.sessionId || null;
-    const scrollDepth: number = Number(body.scrollDepth ?? 0);
+        if (!ua || ua.length < 10) {
+            return new Response("Blocked", { status: 403 });
+        }
 
-    if (!sessionId) {
-      return json({ error: "Missing sessionId" }, 400);
-    }
+        const contentType = request.headers.get("content-type");
 
-    // -----------------------------
-    // Single atomic UPSERT
-    // -----------------------------
-    await env.DB.prepare(`
+        if (!contentType?.includes("application/json")) {
+            return new Response("Invalid", { status: 400 });
+        }
+
+        const body = await request.json();
+
+        const path: string = body.path || "/";
+        const sessionId: string | null = body.sessionId || null;
+        const scrollDepth: number = Number(body.scrollDepth ?? 0);
+
+        if (!sessionId) {
+            return json({ error: "Missing sessionId" }, 400);
+        }
+
+        // -----------------------------
+        // Single atomic UPSERT
+        // -----------------------------
+        await env.DB.prepare(`
       INSERT INTO visits (path, session_id, scroll_depth)
       VALUES (?, ?, ?)
       ON CONFLICT(path, session_id)
       DO UPDATE SET
         scroll_depth = MAX(visits.scroll_depth, excluded.scroll_depth)
     `)
-      .bind(path, sessionId, scrollDepth)
-      .run();
+            .bind(path, sessionId, scrollDepth)
+            .run();
 
-    return json({ success: true });
-  } catch(err) {
-    console.log(err)
-    return json({ error: "Internal error" }, 500);
-  }
+        return json({ success: true });
+    } catch (err) {
+        console.log(err)
+        return json({ error: "Internal error" }, 500);
+    }
 }
 
 /**
  * Helper: JSON response
  */
 function json(data: unknown, status = 200) {
-  return new Response(JSON.stringify(data), {
-    status,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+    return new Response(JSON.stringify(data), {
+        status,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
 }
